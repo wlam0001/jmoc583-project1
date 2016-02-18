ae03b4cb0111324bdec67e2e79bfc336330c3eb6
Emily Lam <wlam0001@live.unc.edu>
2016-02-18 13:07:19 -0500

    Redo of table with single array and display of basic information

--- js/senators.js
+++ js/senators.js
@@ -1,26 +1,26 @@
 var app = angular.module('senators', []);
 
-app.controller('SenatorsController', ['$http', function($http) {
+app.controller('SenatorsController', ['$http', "$scope",function($http, $scope) {
   this.senators = [];
   var _this = this;
 
+  $scope.selected = {};
 
+    $scope.selectPerson = function (person) {
+        $scope.selected.senator = person;
+
+    };
   $http.get('/js/senator.json')
     .success(function(data) {
         console.log(data);
         console.log(this);
-      //   for (var i = 0; i < 100; i++ ) {
-      //  if (i % 7 == 0) _this
-      //  .senators.push([]);
-      //    _this.senators[ _this.senators.length-1].push(data[i]);
-      //   }
-        for (var i = 0; i < 100; i++) {
-            if (i % 10 == 0) {
-              _this.senators.push([]);
-            }
-            _this.senators[_this.senators.length-1].push(data[i]);
-        // _this.senators = data;
-      }
+      //   for (var i = 0; i < 100; i++) {
+      //       if (i % 10 == 0) {
+      //         _this.senators.push([]);
+      //       }
+      //       _this.senators[_this.senators.length-1].push(data[i]);
+      // }
+      _this.senators = data;
     })
     .error(function(msg) {
         console.log("This request failed.\n" + msg);
